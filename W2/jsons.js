c82d8601d5d4200e288cadbc0248d1e10bc2ce68
Mona Heinola <c3hemo00@students.oamk.fi>
2026-09-04 09:50:45 +0300

    W2 json-demo

--- W2/jsons.js
+++ W2/jsons.js
@@ -29,7 +29,25 @@ console.log(henkilo2);
 console.log(ryhma);
 
 //tähän voitaisiin tehdä loop joka käy läpit kaikki lapset
-console.log(ryhma.lapset[0].nimi);
+//console.log(ryhma.lapset[0].nimi);
+
+function lastenNimet(ryhma){
+    for(let i = 0; i < ryhma.lapset.length; i++){
+        console.log(ryhma.lapset[i].nimi)
+    }
+}
+
+lastenNimet(ryhma);
 
 //ja tähän sama, joka laskee sitten iät yhteen ja jakaa nimien määrällä = iän keskiarvo
-console.log(ryhma.lapset[0].ika);
+//console.log(ryhma.lapset[0].ika);
+
+function iatKeskiarvo(ryhma){
+    let sum = 0;
+    for(let i = 0; i < ryhma.lapset.length; i++){
+        sum += + ryhma.lapset[i].ika;
+    }
+    return sum/ryhma.lapset.length;
+}
+
+console.log(iatKeskiarvo(ryhma));
